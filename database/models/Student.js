15fde2331668ec485c48ee384c2c822688b1d6b9
lishannon <lishannon490@gmail.com>
2021-12-13 22:12:16 -0500

    Added defaultValue image for students

--- database/models/Student.js
+++ database/models/Student.js
@@ -17,8 +17,12 @@ const Student = db.define("student", {
   email: {
     type: Sequelize.STRING,
     allowNull: false
-  }
+  },
 
+  imageURL: {
+    type: Sequelize.STRING,
+    defaultValue: "https://media.istockphoto.com/vectors/young-teenage-woman-uses-laptop-for-work-or-chatting-with-friends-vector-id1188816693?k=20&m=1188816693&s=612x612&w=0&h=WIQcHnSo8kDmWBdBYcd8wX8PdJ6alsDYb4LQYRcVRxM="
+  }
 });
 
 
